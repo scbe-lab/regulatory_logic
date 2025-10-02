979dc3b1bae5c8d2f2cbbc475db3a0f8d113b3d4
apposada <apposada@gmail.com>
2025-10-02 12:50:45 +0100

    update_oct25_code

--- outputs/bulk_atac/peakcalling/commands_peakcalling.sh
+++
@@ -1,3 +0,0 @@
-macs2 callpeak -f BED -t ../nucf_trim100bp/atac_vir/atac_vir_nucfree.bed --nomodel --extsize 100 --shift 45 --buffer-size 50000 -g 840213658 -p 0.001 -n atac_vir_macs2peaks --outdir ./macs2_out/
-cat macs2_out/atac_vir_macs2peaks_peaks.narrowPeak | mergeBed -d 20 | sortBed | awk 'BEGIN {OFS="\t"} {print $1,$2,$3,"peak_"NR}' > peaks_virginia.bed
-
--- outputs/motif_analyses/wgcna/homer/command.sh
+++
@@ -1,1 +0,0 @@
-bash spawn_homer.sh promoters ../../associate_peaks_genes/promoters.bed promoters/results
